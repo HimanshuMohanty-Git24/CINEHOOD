96258ee122625ae5682e477843b907e7bc03cc52
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-12-16 22:04:30 +0530

    Work:File and Folder Structure initialized

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Actors from './Actors/Actors';
+import NavBar from './NavBar/NavBar';
+import MovieInformation from './MovieInformation/MovieInformation';
+import Profile from './Profile/Profile';
+import Movies from './Movies/Movies';
+
+export {Actors,NavBar,MovieInformation,Profile,Movies};
