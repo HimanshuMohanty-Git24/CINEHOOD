5f7325fa21bb25b88e9fee1e513112cd852c2d4d
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-12-17 10:12:35 +0530

    Refactor App and NavBar components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,36 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
-import { Route,Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import { Route, Switch } from 'react-router-dom';
 
-import {Actors,NavBar,MovieInformation,Profile,Movies} from './';
+import useStyles from './styles';
+import { Actors, MovieInformation, Movies, Navbar, Profile } from '.';
 
-function App() {
+const App = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-    <CssBaseline/>
-    <NavBar/>
-     <main>
-      <Switch>
-        <Route exact path="/movie/:id">
-          <MovieInformation/>
-        </Route>
-        <Route exact path="/actors/:id">
-          <Actors/>
-        </Route>
-        <Route exact path="/">
-          <Movies/>
-        </Route>
-        <Route exact path="/profile/:id">
-          <Profile/>
-        </Route>
-      </Switch>
-     </main>
+    <div className={classes.root}>
+      <CssBaseline />
+      <Navbar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route exact path={["/", "/approved"]}>
+            <Movies />
+          </Route>
+          <Route exact path="/movie/:id">
+            <MovieInformation />
+          </Route>
+          <Route exact path="/actor/:id">
+            <Actors />
+          </Route>
+          <Route exact path="/profile/:id">
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { AppBar, IconButton, Toolbar, Drawer,Button,Avatar } from '@mui/material'
+import { Menu, AccountCircle, Brightness4, Brightness7 } from '@mui/icons-material'
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <>
+      <AppBar position='fixed'>
+        <Toolbar className={classes.toolbar}></Toolbar>
+      </AppBar>
+    </>
   )
 }
 
