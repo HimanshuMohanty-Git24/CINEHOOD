9295a8a4df2a4c611daa75da03b05d9c01bfbfd0
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-12-22 00:10:07 +0530

    Add Movie, MovieList, and styles for Movie and MovieList components

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,18 +1,40 @@
-import React, { useState, useEffect } from "react";
-import {
-  Box,
-  CircularProgress,
-  useMediaQuery,
-  Typography,
-} from "@mui/material";
-import { useSelector } from "react-redux";
+import React from "react";
+import { Box, CircularProgress, Typography } from "@mui/material";
 import { useGetMoviesQuery } from "../../services/TMDB";
+import MovieList from "../MovieList/MovieList"; // Update the import path
 
 const Movies = () => {
-  const { data } = useGetMoviesQuery();
+  const { data, error, isFetching } = useGetMoviesQuery();
 
-  console.log(data);
-  return <div>Movies</div>;
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="4rem" />
+      </Box>
+    );
+  }
+
+  if (!data || !data.results || data.results.length === 0) {
+    return (
+      <Box display="flex" alignItems="center" mt="20px">
+        <Typography variant="h4">
+          No movies that matched the name.
+          <br />
+          Please search for something else.
+        </Typography>
+      </Box>
+    );
+  }
+
+  if (error) {
+    return 'An error has occurred.';
+  }
+
+  return (
+    <div>
+      <MovieList movies={data} /> {/* Access 'results' property */}
+    </div>
+  );
 };
 
 export default Movies;
--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,19 @@
-import Actors from './Actors/Actors';
-import NavBar from './NavBar/NavBar';
-import MovieInformation from './MovieInformation/MovieInformation';
-import Profile from './Profile/Profile';
-import Movies from './Movies/Movies';
-import Sidebar from './Sidebar/Sidebar';
+import Actors from "./Actors/Actors";
+import NavBar from "./NavBar/NavBar";
+import MovieInformation from "./MovieInformation/MovieInformation";
+import Profile from "./Profile/Profile";
+import Movies from "./Movies/Movies";
+import Sidebar from "./Sidebar/Sidebar";
+import MovieList from "./MovieList/MovieList";
+import Movie from "./Movie/Movie";
 
-export {Actors,NavBar,MovieInformation,Profile,Movies,Sidebar};
+export {
+  Actors,
+  NavBar,
+  MovieInformation,
+  Profile,
+  Movies,
+  Sidebar,
+  MovieList,
+  Movie,
+};
