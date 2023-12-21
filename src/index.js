89456776109b3e813534e157e7cbd9272e759eba
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-12-21 23:21:19 +0530

    Add react-query and configure Redux store

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';  // Import from 'react-dom' instead of 'react-dom/client'
-import App from './components/App';
-import { BrowserRouter } from 'react-router-dom';
-import { createTheme,ThemeProvider } from '@mui/material';
-
+import React from "react";
+import ReactDOM from "react-dom"; // Import from 'react-dom' instead of 'react-dom/client'
+import App from "./components/App";
+import { BrowserRouter } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { Provider } from "react-redux";
+import store from "./app/store";
 const theme = createTheme({});
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-  </ThemeProvider>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </Provider>,
+  document.getElementById("root")
 );
