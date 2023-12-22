fe6bce761bc7e87ddab94ad38614c16164da3fea
HimanshuMohanty-Git24 <codehimanshu24@gmail.com>
2023-12-22 13:07:06 +0530

    Add new images and implement genreOrCategory feature

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -6,24 +6,23 @@ import {
   ListSubheader,
   ListItemIcon,
   Box,
-  CicularProgress,
+  CircularProgress,
   ListItemText,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import useStyles from "./styles";
 import { useTheme } from "@mui/material/styles";
-
+import { useGetGenresQuery } from "../../services/TMDB";
+import genreIcons from '../../assets/genres and categories'
+import { useDispatch, useSelector } from "react-redux";
+import { selectGenreOrCategory } from "../../features/currentGenreOrCategory"
 const categories = [
   { label: "Popular", value: "popular" },
   { label: "Top Rated", value: "top_rated" },
   { label: "Upcoming", value: "upcoming" },
 ];
-const demoCategories = [
-  { label: "Comedy", value: "comedy" },
-  { label: "Action", value: "action" },
-  { label: "Horror", value: "horror" },
-  { label: "Animation", value: "animation" },
-];
+
+
 
 const blueLogo =
   "https://fontmeme.com/permalink/210930/8531c658a743debe1e1aa1a2fc82006e.png";
@@ -33,6 +32,9 @@ const redLogo =
 const Sidebar = ({ setMobileOpen }) => {
   const classes = useStyles();
   const theme = useTheme();
+  const {data,isFetching} = useGetGenresQuery();
+  const dispatch =useDispatch();
+  console.log(data);
   return (
     <div>
       <>
@@ -48,32 +50,40 @@ const Sidebar = ({ setMobileOpen }) => {
           <ListSubheader>Categories</ListSubheader>
           {categories.map(({ label, value }) => (
             <Link key={value} className={classes.links} to='/'>
-              {/* <ListItem onClick={() => {}} button>
-                <img
-                  src={redLogo}
+            <ListItem onClick={() => dispatch(selectGenreOrCategory(value))} button>
+              <ListItemIcon>
+              <img
+                  src={genreIcons[label.toLowerCase()]}
                   className={classes.genereImages}
                   height={30}
                   alt='Category type'
                 />
-              </ListItem> */}
-              <ListItemText primary={label} />
+              </ListItemIcon>
+                <ListItemText primary={label} />
+              </ListItem>
             </Link>
           ))}
         </List>
         <Divider />
         <List>
           <ListSubheader>Genres</ListSubheader>
-          {demoCategories.map(({ label, value }) => (
-            <Link key={value} className={classes.links} to='/'>
-              {/* <ListItem onClick={() => {}} button>
+          {isFetching ? (
+            <Box display="flex" justifyContent="center">
+                <CircularProgress />
+              </Box>
+          ) : data.genres.map(({ name, id }) => (
+            <Link key={name} className={classes.links} to='/'>
+              <ListItem onClick={() => dispatch(selectGenreOrCategory(id))} button>
+                <ListItemIcon>
                 <img
-                  src={redLogo}
+                  src={genreIcons[name.toLowerCase()]}
                   className={classes.genereImages}
                   height={30}
                   alt='Category type'
                 />
-              </ListItem> */}
-              <ListItemText primary={label} />
+                </ListItemIcon>
+                <ListItemText primary={name} />
+              </ListItem>
             </Link>
           ))}
         </List>
--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -6,11 +6,15 @@ export const tmdbApi = createApi({
   reducerPath: "tmdbApi",
   baseQuery: fetchBaseQuery({ baseUrl: "https://api.themoviedb.org/3" }),
   endpoints: (builder) => ({
-    //Get Movies by [Type]
+    //* Get Genres
+    getGenres: builder.query({
+      query: () => `genre/movie/list?api_key=${tmdbApiKey}`,
+    }),
+    //*Get Movies by [Type]
     getMovies: builder.query({
       query: () => `movie/popular?page=${page}&api_key=${tmdbApiKey}`,
     }),
   }),
 });
 
-export const { useGetMoviesQuery } = tmdbApi;
+export const { useGetMoviesQuery, useGetGenresQuery } = tmdbApi;
